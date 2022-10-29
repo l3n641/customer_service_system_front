e9ec62e56f073e8334d35c77ab6ea9f26b85853c
l3n641 <l3n641@192.168.10.7>
2022-10-29 15:24:02 +0800

    修改登录文案

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login-wrap">
     <div class="ms-login">
-      <div class="ms-title">站群后台管理</div>
+      <div class="ms-title">客户管理系统</div>
       <el-form :model="param" :rules="rules" ref="login" label-width="0px" class="ms-content">
         <el-form-item prop="username">
           <el-input v-model="param.username" placeholder="username">
